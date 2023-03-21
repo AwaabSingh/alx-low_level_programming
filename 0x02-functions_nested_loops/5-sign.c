8d38b8ab9895bf56117964059a7068adc4c391a9
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2023-03-21 11:02:55 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 
 /**
  * print_sign - Print the sign of a number
- * @n: The number to be checked 
- * Return: 1 for poitive num, -1 for negative num or zero for anything else
+ * @n: The number to be checked
+ *
+ * Return: 1 for positive number, -1 for negative number, or 0 for zero
  */
 
 int print_sign(int n)
-
-{ 
-	
-            if(n > 0)
-             {
-             _putchar(43);
-             return(1);
-             } 
-             else if (n < 0) 
-             {
-             _putchar(45);
-             return(-1);
-             }
-             else
-	     {
-             _putchar(48);
-             return(0);
-             }
+{
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n < 0)
+    {
+        _putchar('-');
+        return (-1);
+    }
+    else
+    {
+        _putchar('0');
+        return (0);
+    }
 }
+
