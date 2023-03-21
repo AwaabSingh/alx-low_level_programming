5c8e01968b7cc8388ca68520b60db14c2b2bef06
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2023-03-21 13:16:33 -0700

    Task 5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,23 +6,22 @@
  *
  * Return: 1 for positive number, -1 for negative number, or 0 for zero
  */
-
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
-    else
-    {
-        _putchar('0');
-        return (0);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
 
